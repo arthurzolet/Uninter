dbd16a721aa5b90e308b0569c706ef6ecc9f51dc
z0let <64865511+z0let@users.noreply.github.com>
2020-07-13 17:37:14 -0300

    Add files via upload

--- com.uninter/com.uninter.matriz/Matriz.c
+++ com.uninter/com.uninter.matriz/Matriz.c
@@ -5,14 +5,50 @@
 int main(int argc, char const *argv[])
 {
 
-int a [6] = {1, 2, 3, 4};
+int m = 0, n = 0;
+char string[m] [n];
 
-for (size_t i = 0; i < (sizeof a / sizeof a[0]); i++)
+do
 {
-    printf("%d\n", a[i]);
+    printf("Digite o valor do eixo ''m'' (entre ''1'' e ''10''): ");
+    scanf("%d", &m);
+}
+while (m < 1 || m > 10);
+
+printf("\n");
+
+do
+{
+    printf("Digite o valor do eixo ''n'' (entre ''1'' e ''10''): ");
+    scanf("%d", &n);
+}
+while (n < 1 || n > 10);
+
+printf("\n");
+
+for (size_t i = 0; i < m; i++)
+{
+    printf("Digite a %da palavra: ", (i + 1) );
+    scanf("%s", &string[i]);
+    //printf("\n");
+}
+
+for (size_t i = 0; i < m; i++)
+{
+    printf("\n%s", string[i] );
 }
 
 
+/*
+printf("Digite: ");
+//scanf("%s", &string);
+gets(string);
+printf("%s\n", string);
+
+string[3];
+
+printf("%s\n", string);
+*/
 
 printf("\nFIM!!!\n");
 system ("PAUSE");
