1b3a918740ce216a9465018694af32448068d459
z0let <64865511+z0let@users.noreply.github.com>
2020-08-03 17:37:14 -0300

    Update Teste1.c

--- com.uninter/com.uninter.testes/Teste1.c
+++ com.uninter/com.uninter.testes/Teste1.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//teste github
 
 int main(int argc, char const *argv[])
 {
 
-int chuva [] = {0, 838, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0};
-int consumo [] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int chuva[] = {0, 838, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0};
+    int consumo[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-//printf("%lf\n", chuva[i]);
+    //printf("%lf\n", chuva[i]);
 
-for (int i = 0; i < (sizeof (chuva) /sizeof (chuva[0])); i++) {
-    printf("%d\n", chuva[i]);
-}
+    for (int i = 0; i < (sizeof(chuva) / sizeof(chuva[0])); i++)
+    {
+        printf("%d\n", chuva[i]);
+    }
 
-int a = 8;
-printf("\n\n%d\n\n", a);
+    int a = 8;
+    printf("\n\n%d\n\n", a);
 
-printf("\nFIM!!!\n");
-system ("PAUSE");
-return 0;
-
+    printf("\nFIM!!!\n");
+    system("PAUSE");
+    return 0;
 }
