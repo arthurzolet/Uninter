c8e5ce713f7e398091530ff22233b93d8a858ab8
z0let <arthur.zolet@hotmail.com>
2020-11-26 15:10:32 -0300

    .

--- com.uninter/com.uninter.formula/Formula.c
+++ com.uninter/com.uninter.formula/Formula.c
@@ -9,19 +9,43 @@ int main(int argc, char const *argv[])
         int x, y;
     };
 
-    struct ponto2d ponto_inicial;
-    struct ponto2d ponto_final;
-
+    struct ponto2d ponto_inicial, ponto_final;
     int input;
-    printf("[1]-Digitar os valores do primeiro ponto\n[2]-Digitar os valores do segundo ponto\n[3]-Mostrar a distância entre os pontos\n[4]-Sair\n\nEntrada: ");
+
+    printf("[1]-Digitar os valores do primeiro ponto\n[2]-Digitar os valores do segundo ponto\n[3]-Mostrar a distancia entre os pontos\n[4]-Sair\nEntrada: ");
     scanf("%d", &input);
 
-    if (input == 1)
+    while (input != 4)
     {
-        printf("Digite o valor de ''X'' do ponto inicial: ");
-        scanf("%d", )
+        if (input == 1)
+        {
+            printf("Digite o valor de ''X'' do ponto inicial: ");
+            scanf("%d", &ponto_inicial.x);
+            printf("Digite o valor de ''Y'' do ponto inicial: ");
+            scanf("%d", &ponto_inicial.y);
+        }
+        else if (input == 2)
+        {
+            printf("Digite o valor de ''X'' do ponto final: ");
+            scanf("%d", &ponto_final.x);
+            printf("Digite o valor de ''Y'' do ponto final: ");
+            scanf("%d", &ponto_final.y);
+        }
+        else if (input == 3)
+        {
+            int powerX = pow((ponto_inicial.x - ponto_final.x), 2),
+                powerY = pow((ponto_inicial.y - ponto_final.y), 2);
+            double distance = sqrt(powerX + powerY);
+            printf("A distancia entre os pontos eh: %.2f\n", distance);
+        }
+        else
+        {
+            printf("Entrada invalida, digite novamente.\n");
+        }        
+        
+        printf("\n[1]-Digitar os valores do primeiro ponto\n[2]-Digitar os valores do segundo ponto\n[3]-Mostrar a distancia entre os pontos\n[4]-Sair\nEntrada: ");
+        scanf("%d", &input);
     }
-    
 
     printf("\n");
     system("PAUSE");
