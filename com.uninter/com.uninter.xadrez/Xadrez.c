6952c246d78560d7a68d2db362035bece4b3ed5c
z0let <arthur.zolet@hotmail.com>
2020-11-30 15:32:27 -0300

    .

--- com.uninter/com.uninter.xadrez/Xadrez.c
+++ com.uninter/com.uninter.xadrez/Xadrez.c
@@ -3,6 +3,19 @@
 
 int main(int argc, char const *argv[])
 {
+    int tabuleiro[8][8];
+
+    for (size_t i = 0; i < 8; i++)
+    {
+        for (size_t j = 0; j < 8; j++)
+        {
+            tabuleiro[i][j] = 0;            
+            printf(" %d", tabuleiro[i][j]);
+        }                
+        printf("\n");
+    }
+
+    printf("\n\nFIM!!!\n");
     system("PAUSE");
     return 0;
 }
